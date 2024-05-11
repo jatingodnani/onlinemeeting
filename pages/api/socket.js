5f1f3b3e44152fd2c9e9464ecc86825b5f89c306
jatingodnani <godnanijatin@gmail.com>
2024-05-11 21:10:50 +0530

    done

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,57 +1,14 @@
-import { Server } from "socket.io";
-const SOCKET_PORT = 3001;
+import { Server } from 'socket.io'
+
 const SocketHandler = (req, res) => {
   if (res.socket.server.io) {
-    res.status(200).json({
-      success: true,
-      message: "Socket is already running",
-      socket: `:${SOCKET_PORT}`,
-    });
-    return;
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
   }
-  console.log("[SOCKET INITIALIZING 🟡]");
-  const io = new Server({
-    path: "/api/socket",
-    addTrailingSlash: false,
-    cors: { origin: "*" },
-  }).listen(SOCKET_PORT);
+  res.end()
+}
 
-  // Connection
-  io.on("connection", (socket) => {
-  
-    console.log("[CONNECTION]");
-
-    // Join-room
-    socket?.on("join-room", (roomid, id) => {
-      console.log("[JOIN-ROOM]");
-      socket.join(roomid);
-      socket.broadcast.to(roomid).emit("user-connected", id);
-    });
-
-    // Update User
-    socket.on("update-user", (id, roomid, status) => {
-      console.log("[UPDATE-USER]");
-      socket.broadcast.to(roomid).emit("update-other-user", id, status);
-    });
-    // Update User
-    socket.on("leave-chat", (id, roomid) => {
-      console.log("[Leave-chat]");
-      socket.broadcast.to(roomid).emit("left-chat", id);
-    });
-
-    // Disconnection
-    socket.on("disconnect", () => {
-      console.log("[DISCONNECT]");
-    });
-  });
-
-  console.log("[SOCKET STARTED 👍]");
-  res.socket.server.io = io;
-  res.status(201).json({
-    success: true,
-    message: "Socket is started",
-    socket: `:${SOCKET_PORT}`,
-  });
-};
-
-export default SocketHandler;
+export default SocketHandler
